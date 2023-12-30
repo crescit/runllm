31d4c12ab72ceeeae84ae284c855793ea9f7f09c
Mitchell Gillogly <gilloglymg@gmail.com>
2023-12-30 16:43:57 -0600

    2nd commit

--- webapp/next.config.js
+++ webapp/next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        NEXT_PUBLIC_LANGSERVER_URL: process.env.NEXT_PUBLIC_API_URL,
-        NEXT_PUBLIC_APISERVER_URL: process.env.NEXT_PUBLIC_API_KEY,
+        NEXT_PUBLIC_LANGSERVER_URL: "process.env.NEXT_PUBLIC_API_URL",
+        NEXT_PUBLIC_APISERVER_URL: "process.env.NEXT_PUBLIC_API_KEY",
     },
 }
 
