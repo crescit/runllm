32055d291514dfe12b0e162eaf2297a8ef549ac5
caller_unknown <joshjaquez2020@gmail.com>
2024-01-13 11:18:35 -0800

    add error handling to dropbox

--- webapp/src/app/directorypicker.jsx
+++ webapp/src/app/directorypicker.jsx
@@ -4,15 +4,19 @@ import axios from 'axios';
 import { langServerUrl  } from './globals';
 
 const AcceptMaxFiles = ({ file_type, userID, jobTitle, companyName, userName, ...props }) => {
+  const [uploadStatus, setUploadStatus] = useState('idle');
   const [FILETYPE, setFile] = useState()
   const [JOBTITLE, setJobTitle] = useState()
   const [USERNAME, setUsername] = useState()
   const [USERID, setUserID] = useState()
+  const [COMPANYNAME, setCompanyName] = useState()
+  const [errorMsg, setError] = useState('Error uploading file. Please try again.')
 
   useEffect(() => {
     setJobTitle(jobTitle + ', ' + companyName)
     setUsername(userName)
     setUserID(userID)
+    setCompanyName(companyName)
   }, [jobTitle, companyName, userName, userID])
 
   useEffect(() => {
@@ -20,55 +24,75 @@ const AcceptMaxFiles = ({ file_type, userID, jobTitle, companyName, userName, ..
   }, [file_type]);
   
   const onDrop = useCallback((acceptedFiles) => {
+    setUploadStatus('idle'); // Reset upload status
+
+    if (!USERID || JOBTITLE == 'e.g. Software Engineer' || COMPANYNAME == 'e.g. Microsoft' || USERNAME == 'e.g. John Doe') {
+      setError('Missing required fields. Please fill in interview fields.');
+      setUploadStatus('error');
+      return;
+    }
+
     acceptedFiles.forEach((file) => {
       const formData = new FormData();
       formData.append('file', file);
-      // formData.append('user_id', userID)
-      // formData.append('job_title', jobTitle + ', ' + companyName)
-      // formData.append('user_name', userName)
-      formData.append('user_id', USERID)
-      formData.append('job_title', JOBTITLE)
-      formData.append('user_name', USERNAME)
-      console.log(langServerUrl, FILETYPE, file_type);
+      formData.append('user_id', USERID);
+      formData.append('job_title', JOBTITLE);
+      formData.append('user_name', USERNAME);
 
       let path = `${langServerUrl}/write_job_file`;
       if (file_type === 'RESUME') {
         path = `${langServerUrl}/write_resume_file`;
       }
 
-      axios.post(path, formData)
+      axios
+        .post(path, formData)
         .then((response) => {
           console.log('File upload successful:', response.data);
+          setUploadStatus('success');
         })
         .catch((error) => {
           console.error('Error uploading file:', error);
+          setUploadStatus('error');
         });
     });
-  }, [FILETYPE, file_type, USERID, JOBTITLE, USERNAME]);
-
+  }, [file_type, USERID, JOBTITLE, COMPANYNAME, USERNAME]);
 
   const {
     acceptedFiles,
     fileRejections,
     getRootProps,
-    getInputProps
-  } = useDropzone({    
-    maxFiles:1,
+    getInputProps,
+  } = useDropzone({
+    maxFiles: 1,
     accept: {
       'text/plain': [],
-      'application/pdf': []
+      'application/pdf': [],
     },
-    onDrop
+    onDrop,
   });
 
   return (
     <section className="container">
-      <div role="button" tabIndex="0" aria-label="File Drop Zone" {...getRootProps({ role: 'button', className: 'dropzone' })}>
-        <input {...getInputProps()} />
-        <p>Drag 'n' drop file, or click here</p>
-      </div>
+      {uploadStatus === 'idle' ? (
+        <div role="button" tabIndex="0" aria-label="File Drop Zone" {...getRootProps({ role: 'button', className: 'dropzone' })}>
+          <input {...getInputProps()} />
+          <p>Drag 'n' drop file, or click here</p>
+        </div>
+      ) : uploadStatus === 'success' ? (
+        <div>
+          <p>File uploaded successfully!</p>
+        </div>
+      ) : (
+        <div>
+          <div role="button" tabIndex="0" aria-label="File Drop Zone" {...getRootProps({ role: 'button', className: 'dropzone' })}>
+            <input {...getInputProps()} />
+            <p>Drag 'n' drop file, or click here</p>
+          </div>
+          <p style={{ 'color': 'red' }}>{errorMsg}</p>
+        </div>
+      )}
     </section>
   );
-}
+};
 
 export default AcceptMaxFiles;
